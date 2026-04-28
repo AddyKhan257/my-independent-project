c389993d8458ca861a4bb1b687b922bf2b91f573
MOHAMMAD ADNAN KHAN <153667341+AddyKhan257@users.noreply.github.com>
2026-04-28 21:20:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ point.round();
 # round lies in formation gummy it for the one
 # 11 out format 
 # deployment is first for the version of  random release 
-# request is always promoted to first term of        code is never been rejected initially stage composition staggin problem 📈 on mix of two  integers
+# requested is always promoted to first term of        code is never been rejected initially stage composition staggin problem 📈 on mix of two  integers
 
 for i = 0
     j = i+ 10 
