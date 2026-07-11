1446dcf84ed22829638c2e7f24c9ffcf27dd4b19
MOHAMMAD ADNAN KHAN <153667341+AddyKhan257@users.noreply.github.com>
2026-07-11 22:15:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ point.rouzànd();
 # rmaw as ron nnbbhh of the changes mandantory
 # roues in formation guhhmrudmy it fo
  bbonod opportunitygjjgghhhoo
-not yyrooyugtr. round (${git.yoken})
+not yyrooyugtr. round (${git.yokenlu)
 # depmazloymffrfent is first fohhr the rsion of  random h bbbojjom 
 #  is alwllays prom🫡oted to first term of goated form form
 code is never been rejected initially stage compositiokkrn staggin  📈 on mix of two  intege
