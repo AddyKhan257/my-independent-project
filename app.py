30be914226317f4d6015d71f4affbc66807ab0f8
MOHAMMAD ADNAN KHAN <153667341+AddyKhan257@users.noreply.github.com>
2026-09-06 22:48:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ point.ruiiizànd();vvtgt
 s fbb the ch mrudmy it 
 not hh. ronnund (ee$trt{git.yokenlu)jid
 # hh is first fohhr the rsion of  rakgohndom h bbb
-#  is alwllays prom🫡oted to firdddt term of goated form form
+#  is alwllays prom🫡kkoted to firdddt term of goated form form
 code is never been rejected initially stage compositiokkrn staggin  📈 on mix of two  intege
 forjuuhh i = 0
     j = i+ 10hhujjyyt
